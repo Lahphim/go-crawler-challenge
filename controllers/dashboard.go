2211f239167b8f8589581ad1ac40628c9bd8d041
Somsak Arnon <lahphim@gmail.com>
2021-03-07 00:46:28 +0700

    Rollback some codebase in dashboard

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"fmt"
 	"html/template"
+	"net/http"
 
+	form "go-crawler-challenge/forms/scraper"
 	"go-crawler-challenge/models"
+	"go-crawler-challenge/services/scraper"
 
 	"github.com/beego/beego/v2/adapter/context"
 	"github.com/beego/beego/v2/adapter/utils/pagination"
@@ -25,11 +28,15 @@ func (c *DashboardController) NestPrepare() {
 // URLMapping maps dashboard controller actions to functions
 func (c *DashboardController) URLMapping() {
 	c.Mapping("Index", c.Index)
+	c.Mapping("TextSearch", c.TextSearch)
+	c.Mapping("FileSearch", c.FileSearch)
 }
 
 // actionPolicyMapping maps dashboard controller actions to policies
 func (c *DashboardController) actionPolicyMapping() {
 	c.MappingPolicy("Index", Policy{requireAuthenticatedUser: true})
+	c.MappingPolicy("TextSearch", Policy{requireAuthenticatedUser: true})
+	c.MappingPolicy("FileSearch", Policy{requireAuthenticatedUser: true})
 }
 
 // Index handles dashboard with handy widgets
@@ -60,4 +67,56 @@ func (c *DashboardController) Index() {
 	c.Data["XSRFData"] = template.HTML(c.XSRFFormHTML())
 	c.Layout = "layouts/application.html"
 	c.TplName = "dashboard/index.html"
+}
+
+// TextSearch handles keyword for scrapping
+// @Title TextSearch
+// @Description create a new scrapping result by plain text
+// @Success 302 redirect to the dashboard page
+// @Failure 302 redirect to the dashboard page and show an error message
+// @router / [post]
+func (c *DashboardController) TextSearch() {
+	flash := web.NewFlash()
+	textKeywordForm := form.TextKeywordForm{}
+	redirectPath := "/dashboard"
+
+	err := c.ParseForm(&textKeywordForm)
+	if err != nil {
+		flash.Error(err.Error())
+	}
+
+	errors := textKeywordForm.Validate()
+	if len(errors) > 0 {
+		flash.Error(errors[0].Error())
+	} else {
+		positionList, err := models.GetAllPosition()
+		if err != nil {
+			flash.Error(err.Error())
+		} else {
+			searchKeyword := scraper.SearchKeywordService{User: c.CurrentUser, Keyword: textKeywordForm.Keyword}
+			searchKeyword.SetPositionList(positionList)
+			searchKeyword.Run()
+
+			flash.Success("Scraping a keyword :)")
+		}
+	}
+
+	flash.Store(&c.Controller)
+	c.Redirect(redirectPath, http.StatusFound)
+}
+
+// FileSearch handles keyword for scrapping
+// @Title FileSearch
+// @Description create a new scrapping result by CSV file
+// @Success 302 redirect to the dashboard page
+// @Failure 302 redirect to the dashboard page and show an error message
+// @router / [post]
+func (c *DashboardController) FileSearch() {
+	flash := web.NewFlash()
+	redirectPath := "/dashboard"
+
+	flash.Success("Scraping all keywords :)")
+
+	flash.Store(&c.Controller)
+	c.Redirect(redirectPath, http.StatusFound)
 }
