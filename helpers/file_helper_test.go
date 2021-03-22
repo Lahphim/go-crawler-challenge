0f21107ce624c39c9745c047eda57a0f9e7b1229
Somsak Arnon <lahphim@gmail.com>
2021-03-22 16:09:01 +0700

    Add some tests for file helper

---
+++ helpers/file_helper_test.go
@@ -0,0 +1,58 @@
+package helpers_test
+
+import (
+	"fmt"
+	"go-crawler-challenge/helpers"
+	. "go-crawler-challenge/tests"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("FileHelper", func() {
+	Describe("#CheckMatchFileType", func() {
+		Context("given matched file type", func() {
+			It("returns true", func() {
+				_, fileHeader, err := GetMultipartAttributesFromFile("tests/fixtures/files/valid.csv", "text/csv")
+				if err != nil {
+					Fail(fmt.Sprintf("Get multipart atrributes from file failed: %v", err))
+				}
+
+				expectFileTypes := []string{"text/csv"}
+				matchResult := helpers.CheckMatchFileType(fileHeader, expectFileTypes)
+
+				Expect(matchResult).To(BeTrue())
+			})
+		})
+
+		Context("given unmatched file type", func() {
+			It("returns false", func() {
+				_, fileHeader, err := GetMultipartAttributesFromFile("tests/fixtures/files/text.txt", "text/csv")
+				if err != nil {
+					Fail(fmt.Sprintf("Get multipart atrributes from file failed: %v", err))
+				}
+
+				expectFileTypes := []string{"text/csv"}
+				matchResult := helpers.CheckMatchFileType(fileHeader, expectFileTypes)
+
+				Expect(matchResult).To(BeFalse())
+			})
+		})
+	})
+
+	Describe("#ReadFileContent", func() {
+		It("returns list of content", func() {
+			file, _, err := GetMultipartAttributesFromFile("tests/fixtures/files/valid.csv", "text/csv")
+			if err != nil {
+				Fail(fmt.Sprintf("Get multipart atrributes from file failed: %v", err))
+			}
+
+			contentList, err := helpers.ReadFileContent(file)
+			if err != nil {
+				Fail(fmt.Sprintf("Read file content failed: %v", err))
+			}
+
+			Expect(len(contentList)).To(BeNumerically(">", 0))
+		})
+	})
+})
