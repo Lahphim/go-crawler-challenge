13d00d970a97790fb36df678e5362a83cbac2652
Somsak Arnon <lahphim@gmail.com>
2021-03-25 03:59:49 +0700

    Add oauth client controller and page

---
+++ controllers/oauth_client.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	service "go-crawler-challenge/services/oauth"
+
+	"github.com/beego/beego/v2/server/web"
+)
+
+// OauthClientController operations for oauth2 client generator
+type OauthClientController struct {
+	BaseController
+}
+
+// NestPrepare prepares some configurations to the controller
+func (c *OauthClientController) NestPrepare() {
+	c.actionPolicyMapping()
+}
+
+// URLMapping maps oauth2 client controller actions to functions
+func (c *OauthClientController) URLMapping() {
+	c.Mapping("New", c.New)
+	c.Mapping("Create", c.Create)
+}
+
+// actionPolicyMapping maps oauth2 client controller actions to policies
+func (c *OauthClientController) actionPolicyMapping() {
+	c.MappingPolicy("New", Policy{requireAuthenticatedUser: true})
+	c.MappingPolicy("Create", Policy{requireAuthenticatedUser: true})
+}
+
+func (c *OauthClientController) New() {
+	web.ReadFromRequest(&c.Controller)
+	flash := web.NewFlash()
+
+	clientId := c.GetString("client_id")
+	if clientId != "" {
+		oauthClient, err := service.ClientStore.GetByID(context.TODO(), clientId)
+		if err != nil {
+			flash.Error("OAuth client credential not found!")
+		} else {
+			c.Data["ClientId"] = oauthClient.GetID()
+			c.Data["ClientSecret"] = oauthClient.GetSecret()
+		}
+	}
+
+	flash.Store(&c.Controller)
+	c.Data["XSRFForm"] = template.HTML(c.XSRFFormHTML())
+	c.Layout = "layouts/application.html"
+	c.TplName = "oauth_client/new.html"
+}
+
+func (c *OauthClientController) Create() {
+	flash := web.NewFlash()
+	redirectPath := "/oauth_client"
+
+	serviceOauth := service.ClientGenerator{}
+	clientId, err := serviceOauth.Generate()
+	if err != nil {
+		flash.Error(err.Error())
+	} else {
+		flash.Success("You have successfully created a new client")
+		redirectPath = fmt.Sprintf("%v?client_id=%v", redirectPath, clientId)
+	}
+	flash.Success("You have successfully created a new client")
+
+	flash.Store(&c.Controller)
+	c.Redirect(redirectPath, http.StatusFound)
+}
